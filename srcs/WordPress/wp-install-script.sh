b3045f8e4c5bcd5aa3f2baa0db82cddb51ec4a62
max <max@debian.com>
2023-04-10 04:29:29 -0800

    gitignore

---
+++ srcs/WordPress/wp-install-script.sh
@@ -0,0 +1,10 @@
+#!/bin/sh 
+
+wp core download --path="/var/www/html/wordpress" --allow-root
+wp config create --path="/var/www/html/wordpress" --allow-root --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASS
+wp core install --path="/var/www/html/wordpress" --allow-root --url=$DOMAIN_NAME --title=$WP_SITE_TITLE --admin_user=$WP_ADMIN_USER --admin_email=$WP_ADMIN_MAIL --admin_password=$WP_ADMIN_PASSWORD
+wp plugin update --allow-root --all
+wp user create --path="/var/www/html/wordpress" --allow-root $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD
+chown www-data:www-data /var/www/html/wordpress/wp-content/uploads -R
+# mkdir -p /run/php
+# php-fpm7.4 -F
