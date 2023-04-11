11c71b0e76101149da10794ec15b04a3cddc9d28
max <max@debian.com>
2023-04-11 07:51:37 -0800

    feur

--- srcs/WordPress/wp-install-script.sh
+++ srcs/WordPress/wp-install-script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 cd /var/www/html/wordpress
 wp core download --path="/var/www/html/wordpress" --allow-root
-wp config create --path="/var/www/html/wordpress" --allow-root --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASS
+wp config create --path="/var/www/html/wordpress" --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --dbprefix=wp_
 wp core install --path="/var/www/html/wordpress" --allow-root --url=$DOMAIN_NAME --title=$WP_SITE_TITLE --admin_user=$WP_ADMIN_USER --admin_email=$WP_ADMIN_MAIL --admin_password=$WP_ADMIN_PASSWORD
 wp plugin update --allow-root --all
 wp user create --path="/var/www/html/wordpress" --allow-root $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD
